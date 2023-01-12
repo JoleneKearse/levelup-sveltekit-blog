f60e0592381bddbba71c243ebc2cd21c6f707865
Jolene Kearse <pharaohnutz@gmail.com>
2023-01-12 15:04:41 -0500

    add lib

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
+<script>
+  import Test from '$lib/Test.svelte'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <a href="/about">About Us</a>
+
+<Test />
