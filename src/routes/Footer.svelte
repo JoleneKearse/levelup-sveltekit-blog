c0e7b18f7d020ff544d45c6e8df18b1cb38c90f4
Jolene Kearse <pharaohnutz@gmail.com>
2023-01-11 19:37:42 -0500

    feat: add styling for Header, Footer & global

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,14 @@
 <footer>
   <p>&#169; Level Up Tutorials</p>
 </footer>
+
+<style>
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: .5rem;
+    background: #333;
+    color: #fffffe;
+  }
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,14 @@
 <header>
   <h1>Level Up Blog</h1>
 </header>
+
+<style>
+  header {
+    padding: 1rem;
+    background: #333;
+    color: #fffffe;
+  }
+  h1 {
+    font-size: 1em;
+  }
+</style>
