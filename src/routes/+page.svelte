c78977025c7e264fb03c19d5075e0842f89f3e1a
Jolene Kearse <pharaohnutz@gmail.com>
2023-01-14 18:10:03 -0500

    start learning about loading data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script>
   import Test from '$lib/Test.svelte';
-  import '$db/start'
+  import '$db/start';
+  export let data 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>{data.hello}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <a href="/about">About Us</a>
