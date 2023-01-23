6afe8c50dce103c5d28d132f630f2eaeaa5afbdb
Jolene Kearse <pharaohnutz@gmail.com>
2023-01-23 14:22:53 -0500

    feat: use setHeaders for caching

--- src/routes/show/[num]/+page.server.js
+++ src/routes/show/[num]/+page.server.js
@@ -1,6 +1,11 @@
-export async function load({ fetch, params }) {
+export async function load({ fetch, params, setHeaders }) {
 	const res = await fetch(`https://syntax.fm/api/shows/${params.num}`);
 	const data = await res.json();
+
+	setHeaders({
+		'Cache-Control': 'max-age=3600'
+	});
+
 	return {
 		episode: data
 	};
